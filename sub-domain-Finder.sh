f4f2e05f3fdecd6b78659f124e0f5a817c9e00db
ABHISHEK OLI <oliabhi26@gmail.com>
2021-05-29 21:43:51 +0530

    New logic : User will now be prompted to enter the details like domain name,sublist,cpu threads to be created

--- sub-domain-Finder.sh
+++ sub-domain-Finder.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-domain=$1
-list=$2
-noOfThreads=$3
+#domain=$1
+#list=$2
+#noOfThreads=$3
+# Read the user input
 
+echo "Enter the domain name: "
+read domain_name
+echo "Enter List detail names: "
+read listName
+echo "Enter the number of cpu threads: "
+read threadNum
 while read sub;
-do echo "$sub.$domain";
-done < $list  | xargs -P$noOfThreads -I X bash -c "if host X &> '$domain'.txt ;then echo X;fi"
-
-
+do echo "$sub.$domain_name";
+done < $listName  | xargs -P$threadNum -I X bash -c "if host X &> '$domain_Name'.txt ;then echo X;fi"
