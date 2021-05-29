7006f8d9471a149d8725c4f9b73acb86814d325a
ABHISHEK OLI <oliabhi26@gmail.com>
2021-05-29 20:29:31 +0530

    sh code

---
+++ sub-domain-Finder.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+domain=$1
+list=$2
+noOfThreads=$3
+
+while read sub;
+do echo "$sub.$domain";
+done < $list  | xargs -P$noOfThreads -I X bash -c "if host X &> /dev/null;then echo X;fi"
+
