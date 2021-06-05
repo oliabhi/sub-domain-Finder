bd1a9f26a749d15acb3e6692a0d67f7406a5e37e
ABHISHEK OLI <oliabhi26@gmail.com>
2021-06-05 11:13:44 +0530

    cleaning up

--- sub-domain-Finder.sh
+++ sub-domain-Finder.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
-#domain=$1
-#list=$2
-#noOfThreads=$3
-# Read the user input
-
 echo "Enter the domain name: "
-read suraj
-
-# whatever user is entering in the screen will be stored in a variable named suraj
-# $suraj
+read domainName
 echo "Enter List detail names: "
 read listName
 echo "Enter the number of cpu threads: "
 read threadNum
 
-# $ means value of variable
 while read sub;
-do echo "$sub.$suraj";  
-# /dev/null
-done < $listName  | xargs -P$threadNum -I X bash -c "if host X &> '$suraj'.txt ;then echo X;fi"
+do echo "$sub.$domainName";  
+done < $listName  | xargs -P$threadNum -I X bash -c "if host X &> '$domainName'.txt ;then echo X;fi"
